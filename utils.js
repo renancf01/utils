69354f5a4eb384bb82656349c6a1bbdd62adfc7e
Renan Carvalho Ferreira <renancf01@gmail.com>
2020-09-03 23:25:35 -0300

    adjustments in calculations and set reset function

--- utils.js
+++ utils.js
@@ -1,16 +1,15 @@
-window.onload = () => {
+function operation () {
+    const hour1 = document.getElementById('hour1');
+    const minute1 = document.getElementById('minute1');
+    const hour2 = document.getElementById('hour2');
+    const minute2 = document.getElementById('minute2');
+    const resultHour = document.getElementById('resultHour');
+    const reusltMinute = document.getElementById('resultMinute');
 
-    const hour1 = document.getElementById('hour1')
-    const minute1 = document.getElementById('minute1')
-    const hour2 = document.getElementById('hour2')
-    const minute2 = document.getElementById('minute2')
-    const resultHour = document.getElementById('resultHour')
-    const reusltMinute = document.getElementById('resultMinute')
-
-    document.getElementById('sum').addEventListener('click', function () {
-        let totalMinute = parseInt(minute1.value) + parseInt(minute2.value)
-        let totalHour = parseInt(hour1.value) + parseInt(hour2.value)
-
+    document.getElementById('sum').addEventListener('click', () => {
+        let totalMinute = parseInt(minute1.value) + parseInt(minute2.value) || 0
+        let totalHour = parseInt(hour1.value) + parseInt(hour2.value) || 0
+        
         if(totalMinute >= 60) {
             resultHour.value = totalHour + 1
             reusltMinute.value = totalMinute - 60
@@ -18,16 +17,16 @@ window.onload = () => {
             resultHour.value = totalHour
             reusltMinute.value = totalMinute
         }
+        hour1.value = null
+        minute1.value = null
+        hour2.value = null
+        minute2.value = null
+    });
 
-    })
-    document.getElementById('decrease').addEventListener('click', function () {
-        let totalMinute = parseInt(minute1.value) - parseInt(minute2.value)
-        let totalHour = parseInt(hour1.value) - parseInt(hour2.value)
-
-        if(totalMinute == null){
-            resultMinute.value = 0
-        }        
-        
+    document.getElementById('decrease').addEventListener('click', () => {
+        let totalMinute = parseInt(minute1.value) - parseInt(minute2.value) || 0
+        let totalHour = parseInt(hour1.value) - parseInt(hour2.value) || 0
+    
         if(totalMinute < 0) {
             if(totalMinute == 0) {
                 reusltMinute.value = 0
@@ -39,6 +38,19 @@ window.onload = () => {
             resultHour.value = totalHour
             reusltMinute.value = totalMinute
         }
+        hour1.value = null
+        minute1.value = null
+        hour2.value = null
+        minute2.value = null
+        
+    });
+
+    document.getElementById('reset').addEventListener('click', () => {
+        resultHour.value = null
+        reusltMinute.value = null
+    });
     
-    })  
-}
+};
+
+operation()
+
